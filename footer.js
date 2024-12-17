0c9957354743ffc6547d0556eb3c5f5bbb3c6f19
Miguel Nicolau <nicolaum@Maximus.local>
2024-12-17 13:03:27 +0000

    Completely new version of site.

---
+++ footer.js
@@ -0,0 +1,10 @@
+var footer = document.getElementById('footer');
+footer.innerHTML = `
+<div id = 'div_bottom_left'>
+    <a href = 'PrivacyPolicy/2.0/index.html'>Privacy Policy</a>
+    |
+    <a href = 'Licence/2.0/LICENCE.html'>License</a>
+</div>
+<div id = 'div_bottom_right'>info@bsgenerator.org</div>
+`;
+
---
+++ header.js
@@ -0,0 +1,16 @@
+var header = document.getElementById('header');
+header.innerHTML = `
+<div id = 'div_navtitle'>
+    <a href = 'index.html'>
+        <img src = 'images/BSGeneratorLogo.png'/>
+        BSGenerator
+    </a>
+</div>
+<div id = 'div_nav'>
+    <span><a class = 'nav_button' href = 'BSManager.html'>BSManager</a></span>  
+    <span><a class = 'nav_button' href = 'BSEditor.html'>BSEditor</a></span>  
+    <span><a class = 'nav_button' href = 'docs/html/index.html'>Documentation</a></span>  
+    <span><a class = 'dl_button' href = 'download.html'>Download</a></span>
+</div>
+`;
+
