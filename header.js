e58e3b9e548388a28b66f428e958fc10cc99d3a2
Miguel Nicolau <nicolaum@Maximus.local>
2024-12-18 01:53:41 +0000

    Documentation became User Guide.

--- header.js
+++ header.js
@@ -9,7 +9,7 @@ header.innerHTML = `
 <div id = 'div_nav'>
     <span><a class = 'nav_button' href = 'BSManager.html'>BSManager</a></span>  
     <span><a class = 'nav_button' href = 'BSEditor.html'>BSEditor</a></span>  
-    <span><a class = 'nav_button' href = 'docs/html/index.html'>Documentation</a></span>  
+    <span><a class = 'nav_button' href = 'docs/html/index.html'>User Guide</a></span>  
     <span><a class = 'dl_button' href = 'download.html'>Download</a></span>
 </div>
 `;
